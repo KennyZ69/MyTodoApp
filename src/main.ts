faf5851abbefd12b943c6e5d73917c5eb40a1882
KennyZ69 <martin.paulik06@gmail.com>
2024-02-01 18:54:06 +0100

    "just added a close button and worked around a bit but will finish it tomorrow"

--- src/main.ts
+++ src/main.ts
@@ -13,22 +13,41 @@ const initApp = (): void =>{
   const fullList = FullList.instance;
   const template = ListTemplate.instance;
 
+  const titleInput = document.getElementById('title-input') as HTMLInputElement;
+  const dateInput = document.getElementById('date-input') as HTMLInputElement;
+  const descInput = document.getElementById('description-input') as HTMLInputElement;
+
+
   addOrUpdateTask.addEventListener('click', (e): void =>{
     e.preventDefault();
-    const titleInput = document.getElementById('title-input') as HTMLInputElement;
+    // const titleInput = document.getElementById('title-input') as HTMLInputElement;
     const titleValue: string = titleInput.value.trim();
+    // const dateInput = document.getElementById('date-input') as HTMLInputElement;
+    // const dateValue: string = dateInput.value
+    // const descInput = document.getElementById('description-input') as HTMLInputElement;
     if(!titleValue) return
 
     const itemId = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1; 
-    const newItem = new ListItem(itemId.toString(), titleValue);
+    const newItem = new ListItem(itemId.toString(), titleInput.value, dateInput.value, descInput.value);
 
     fullList.addItem(newItem);
     taskForm.classList.toggle("hidden");
     template.render(fullList);
   })
 
+  const closeTask = document.getElementById('close-task-form-btn') as HTMLButtonElement;
+  closeTask.addEventListener('click', (e) => {
+    e.preventDefault();
+    taskForm.classList.toggle('hidden');
+    titleInput.value = "";
+    dateInput.value = "";
+    descInput.value = "";
+  })
+
   addNewTask.addEventListener('click', (e) => {
     e.preventDefault;
+    addOrUpdateTask.textContent = "Add Task";
+    
     taskForm.classList.toggle("hidden");
 })
 fullList.load();
